f0cbfd7cd895634def53a4e6e77457d52bfc939f
Soufiane1Boutouil <141652474+Soufiane1Boutouil@users.noreply.github.com>
2023-08-07 18:19:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// This is the JavaScript code for your portfolio website
-
 // Function to toggle background color between blue and white
 let isBlueBackground = true;
-const toggleBackgroundColor = () => {
+const toggle background color = () => {
   const body = document.body;
   if (isBlueBackground) {
-    body.style.backgroundColor = "white";
+    body. style.backgroundColor = "white";
   } else {
-    body.style.backgroundColor = "lightblue";
+    body. style.backgroundColor = "light blue";
   }
   isBlueBackground = !isBlueBackground;
 };
@@ -27,7 +25,7 @@ const toggleNavVisibility = () => {
 // Event listeners for button clicks
 document.addEventListener("DOMContentLoaded", function() {
   const changeColorButton = document.getElementById("change-color-button");
-  changeColorButton.addEventListener("click", toggleBackgroundColor);
+  changeColorButton.addEventListener("click", toggle background color);
 
   const toggleNavButton = document.getElementById("toggle-nav-button");
   toggleNavButton.addEventListener("click", toggleNavVisibility);
